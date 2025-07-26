b908efa6abd0b4c821c7cb9682e10f015d312e09
Hakob319 <hakobharutyunyan014@gmail.com>
2025-07-26 13:38:12 +0400

    Update pointer_task1.c

--- pointer_task1.c
+++ pointer_task1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main();
+int main(){
 
 int a = 5;
 float b = 3.5;
-char = 'H';
+char = c 'H';
 
 
 int *ptr1 = &a;
@@ -12,9 +12,9 @@ float *ptr2 = &b;
 char *ptr3 = &c;
 
 
-printf("Addres in ptr1 %p", &ptr1);
-printf("Addres in ptr2 %p", &ptr2);
-printf("Addres in ptr3 %p", &ptr3);
+printf("Addres in ptr1 %p", ptr1);
+printf("Addres in ptr2 %p", ptr2);
+printf("Addres in ptr3 %p", ptr3);
 
 return 0;
 
